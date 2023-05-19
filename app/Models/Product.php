9e2423dc496a08aefd4da5fe1143b2d740caf7f2
Your Name <you@example.com>
2023-05-19 11:14:50 +0800

    sdf

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model{
 	use HasFactory;
 	public $timestamps		= false;
 
 	public function getImageAttribute($val){
-		return $val ? explode(',', $val) : [];
+		$res 	= $val ? explode(',', $val) : [];
+		foreach($res as &$item){
+			if($item && substr($item, 0, 4) != 'http'){
+				$item 	= Storage::url($item);
+			}
+		}
+		return $res;
 	}
 
 	public function setImageAttribute($val){
